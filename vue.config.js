882bba0a87c87d4e1a311333071808206ee5d711
luckyRogue <32385682+luckyRogue@users.noreply.github.com>
2020-10-26 10:06:34 +0800

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     // 基本路径
-    publicPath:process.env.NODE_ENV === 'production' ? './' : '/',
+    publicPath:process.env.NODE_ENV === 'production' ? '/' : '/',
     outputDir:"dist",  //打包时生成的生产环境构建文件的目录
     /*devServer: {
         proxy: 'http://localhost:8080'
